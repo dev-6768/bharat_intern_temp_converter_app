bee7002f3c2d2eb9e165e8f37b6e3d15dbd7c6c3
ThinkToGrow <aaradhya.mishra@gmail.com>
2023-08-12 17:49:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/intern_temp_converter_app/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.intern_temp_converter_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editTextDegreeC = findViewById<EditText>(R.id.DegreeCelsius)
+        val editTextDegreeF = findViewById<EditText>(R.id.DegreeFahrenheit)
+        val editTextKelvin = findViewById<EditText>(R.id.Kelvin)
+
+        val actionButton = findViewById<Button>(R.id.actionButton)
+        val clearButton = findViewById<Button>(R.id.clearButton)
+
+        actionButton.setOnClickListener {
+            if(editTextDegreeC.text.toString() != "" && editTextDegreeF.text.toString() == "" && editTextKelvin.text.toString() == "") {
+                val number = editTextDegreeC.text.toString().toDouble()
+                editTextDegreeF.setText("${(9/5)*number + 32} degree F")
+                editTextKelvin.setText("${number + 273} K")
+            }
+
+            else if(editTextDegreeC.text.toString() == "" && editTextDegreeF.text.toString() != "" && editTextKelvin.text.toString() == "") {
+                val number = editTextDegreeF.text.toString().toDouble()
+                editTextDegreeC.setText("${(5/9)*(number - 32)} degree C")
+                editTextKelvin.setText("${(5/9)*(number - 32) + 273} K")
+            }
+
+            else if(editTextDegreeC.text.toString() == "" && editTextDegreeF.text.toString() == "" && editTextKelvin.text.toString() != "") {
+                val number = editTextKelvin.text.toString().toDouble()
+                editTextDegreeC.setText("${number-273} K")
+                editTextDegreeF.setText("${(9/5)*(number - 273) + 32} degree F")
+            }
+
+            else{
+                Toast.makeText(this, "Please fill up the values properly.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        clearButton.setOnClickListener{
+            editTextDegreeC.setText("")
+            editTextDegreeF.setText("")
+            editTextKelvin.setText("")
+        }
+
+    }
+}
